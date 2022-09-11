1b4e4ac36def55b89f6e725021622397423f4391
killthistinker <killthistinker@gmail.com>
2022-09-12 03:14:56 +0600

    #3 Добавил валидацию для аккаунтов

---
+++ FoodDelivery/Controllers/AccountValidationController.cs
@@ -0,0 +1,28 @@
+﻿using System.Linq;
+using FoodDelivery.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FoodDelivery.Controllers
+{
+    public class AccountValidationController : Controller
+    {
+        private readonly DeliveryContext _context;
+
+        public AccountValidationController(DeliveryContext context)
+        {
+            _context = context;
+        }
+
+        public bool CheckAccountEmail(string email)
+        {
+            bool validation = !_context.Users.AsEnumerable().Any(p => p.Email.Equals(email));
+            return validation;
+        }
+
+        public bool CheckAccountName(string userName)
+        {
+            bool validation = !_context.Users.AsEnumerable().Any(p => p.UserName.Equals(userName));
+            return validation;
+        }
+    }
+}
