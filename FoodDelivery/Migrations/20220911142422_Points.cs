9cfbd980adae494c84cbd5ad6c7abc70954ed872
killthistinker <killthistinker@gmail.com>
2022-09-12 03:14:12 +0600

    #3 Обновил миграции

---
+++ FoodDelivery/Migrations/20220911142422_Points.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace FoodDelivery.Migrations
+{
+    public partial class Points : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
