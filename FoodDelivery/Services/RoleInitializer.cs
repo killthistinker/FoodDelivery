37484909335eaf4b775d230c6bb05b09e9559b65
killthistinker <killthistinker@gmail.com>
2022-09-12 03:22:57 +0600

    #3 Добавил инициализатор ролей

---
+++ FoodDelivery/Services/RoleInitializer.cs
@@ -0,0 +1,34 @@
+﻿using System.Threading.Tasks;
+using FoodDelivery.Models;
+using Microsoft.AspNetCore.Identity;
+
+namespace FoodDelivery.Services
+{
+    public class RoleInitializer
+    {
+        public static async Task SeedARoleUser(UserManager<User> userManager, RoleManager<Role> roleManager)
+        {
+            string adminEmail = "admin@gmail.com";
+            string password = "12345";
+            var roles = new[] { "user", "admin", "administrator" };
+
+            foreach (var role in roles)     
+            {
+                if (await roleManager.FindByNameAsync(role) is null)
+                {
+                    await roleManager.CreateAsync(new Role{Name = role});
+                }
+            }
+            
+            if (await userManager.FindByNameAsync(adminEmail) == null)
+            {
+                User admin = new User { Email = adminEmail, UserName = adminEmail, IamAdmin = true};
+                IdentityResult result = await userManager.CreateAsync(admin, password);
+                if (result.Succeeded)
+                {
+                    await userManager.AddToRoleAsync(admin, "admin");
+                }
+            }
+        }
+    }
+}
