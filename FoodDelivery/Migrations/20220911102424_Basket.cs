159460101f29fc51557641df2e21b582e23fe4de
killthistinker <killthistinker@gmail.com>
2022-09-12 03:08:12 +0600

    #3 Обновил миграции

---
+++ FoodDelivery/Migrations/20220911102424_Basket.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace FoodDelivery.Migrations
+{
+    public partial class Basket : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "BasketId",
+                table: "Points",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateTable(
+                name: "Baskets",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Sum = table.Column<decimal>(type: "numeric", nullable: false),
+                    PointId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Baskets", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Points_BasketId",
+                table: "Points",
+                column: "BasketId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Points_Baskets_BasketId",
+                table: "Points",
+                column: "BasketId",
+                principalTable: "Baskets",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Points_Baskets_BasketId",
+                table: "Points");
+
+            migrationBuilder.DropTable(
+                name: "Baskets");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Points_BasketId",
+                table: "Points");
+
+            migrationBuilder.DropColumn(
+                name: "BasketId",
+                table: "Points");
+        }
+    }
+}
