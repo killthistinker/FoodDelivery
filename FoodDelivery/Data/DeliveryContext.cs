c28098cfbd5f6763c56615a9809c7906425d565d
killthistinker <killthistinker@gmail.com>
2022-09-12 03:08:56 +0600

    #3 Обновил миграции

---
+++ FoodDelivery/Data/DeliveryContext.cs
@@ -0,0 +1,17 @@
+﻿using FoodDelivery.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace FoodDelivery.Data
+{
+    public class DeliveryContext : IdentityDbContext<User, Role, int>
+    {
+        public DbSet<Point> Points { get; set; }
+        public DbSet<Dish> Dishes { get; set; }
+        public DbSet<Basket> Baskets { get; set; }
+        public DeliveryContext(DbContextOptions<DeliveryContext> options) : base(options)
+        {
+            
+        }
+    }
+}
