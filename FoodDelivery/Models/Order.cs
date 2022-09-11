2b19268be0ff5220e0be6dfda65604dab74df27a
killthistinker <killthistinker@gmail.com>
2022-09-12 03:22:42 +0600

    #4 Добавил модель заказа

---
+++ FoodDelivery/Models/Order.cs
@@ -0,0 +1,9 @@
+﻿namespace FoodDelivery.Models
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public Dish Dish { get; set; }
+        public int DishId { get; set; }
+    }
+}
