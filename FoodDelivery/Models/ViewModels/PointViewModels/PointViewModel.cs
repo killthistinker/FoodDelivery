ac8b1f8cae4cf05feca7f9c6491d1b998addb26c
killthistinker <killthistinker@gmail.com>
2022-09-12 03:23:24 +0600

    #3 Добавил модель для создания заведения

---
+++ FoodDelivery/Models/ViewModels/PointViewModels/PointViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FoodDelivery.Models.ViewModels.PointViewModels
+{
+    public class PointViewModel
+    {
+        [Display(Name = "Name")]
+        [Required(ErrorMessage = "Поле обязательно для заполнения")]
+        [Remote("CheckAccountName", "AccountValidation", ErrorMessage = "Логин занят")]
+        public string Name { get; set; }
+        
+        [Display(Name = "File")]
+        [Required(ErrorMessage = "Поле обязательно для заполнения")]
+        public IFormFile File { get; set; }
+        public string ImagePath { get; set; }
+        [Display(Name = "Description")]
+        [Required(ErrorMessage = "Поле обязательно для заполнения")]
+        public string Description { get; set; }
+    }
+}
