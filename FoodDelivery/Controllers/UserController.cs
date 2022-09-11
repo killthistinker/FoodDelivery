25ac1fd808c0e84d70a2c0df5fa4130e146dce2e
killthistinker <killthistinker@gmail.com>
2022-09-12 03:07:35 +0600

    #3 Добавил контроллер для управления пользователями

---
+++ FoodDelivery/Controllers/UserController.cs
@@ -0,0 +1,34 @@
+﻿using System.Threading.Tasks;
+using FoodDelivery.Models.ViewModels;
+using FoodDelivery.Services.Abstractions;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FoodDelivery.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly IUserService _service;
+
+        public UserController(IUserService service)
+        {
+            _service = service;
+        }
+
+        [HttpPost]
+        [Authorize(Roles = "admin")]
+        public async Task<IActionResult> AddUser(RegistrationViewModel model)
+        {
+            await _service.AddUser(model);
+            return RedirectToAction("GetUsers", "Manager");
+        }
+
+        [HttpPost]
+        [Authorize(Roles = "admin")]
+        public async Task<IActionResult> DeleteUser(int id)
+        {
+            await _service.DeleteUser(id);
+            return Ok(200);
+        }
+    }
+}
