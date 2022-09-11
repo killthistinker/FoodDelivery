242985dec788554dca3dcd7385698a54d561c531
killthistinker <killthistinker@gmail.com>
2022-09-12 03:16:48 +0600

    #3 Добавил сервис для администратора

---
+++ FoodDelivery/Services/Abstractions/IManagerService.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using FoodDelivery.Models;
+using FoodDelivery.Models.ViewModels;
+using FoodDelivery.Models.ViewModels.PointViewModels;
+
+namespace FoodDelivery.Services.Abstractions
+{
+    public interface IManagerService
+    {
+        public Task AddPoint(PointViewModel model);
+        public Task AddDish(DishViewModel model);
+        public Task DeleteDish(int dishId);
+        public Task DeletePoint(int pointId);
+        public IndexDishViewModel GetAllDishes();
+        public IndexPointViewModel GetAllPoints();
+        public IndexUserViewModel GetAllUsers();
+    }
+}
