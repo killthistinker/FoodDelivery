69ca98bbe3cca8d92b14f9d51f6773903f1bd292
killthistinker <killthistinker@gmail.com>
2022-09-12 03:28:17 +0600

    #4 Добавил контроллер для главной страницы

---
+++ FoodDelivery/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System.Threading.Tasks;
+using FoodDelivery.Services.Abstractions;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FoodDelivery.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IPointService _pointService;
+        public HomeController(IPointService pointService)
+        {
+            _pointService = pointService;
+        }
+
+        public IActionResult Index()
+        {
+            var points = _pointService.GetPoints();
+            return View(points);
+        }
+
+        public async Task<IActionResult> GetPointInfo(int id)
+        {
+            var point = await _pointService.GetPoint(id);
+            return View(point);
+        }
+    }
+}
