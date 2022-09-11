dafbeec841d88d79f74f7214cf789856ac6b42ab
killthistinker <killthistinker@gmail.com>
2022-09-12 03:07:04 +0600

    #3 Создал сервис для добавления пользователей

---
+++ FoodDelivery/Services/Abstractions/IUserService.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading.Tasks;
+using FoodDelivery.Models.ViewModels;
+
+namespace FoodDelivery.Services.Abstractions
+{
+    public interface IUserService
+    {
+        public Task AddUser(RegistrationViewModel model);
+        public Task DeleteUser(int id);
+    }
+}
---
+++ FoodDelivery/Services/UserService.cs
@@ -0,0 +1,42 @@
+﻿using System.Threading.Tasks;
+using FoodDelivery.Helpers;
+using FoodDelivery.Models;
+using FoodDelivery.Models.ViewModels;
+using FoodDelivery.Repository.Interfaces;
+using FoodDelivery.Services.Abstractions;
+using Microsoft.AspNetCore.Identity;
+
+namespace FoodDelivery.Services
+{
+    public class UserService : IUserService
+    {
+        private readonly IUserRepository _repository;
+        private readonly UserManager<User> _userManager;
+
+        public UserService(IUserRepository repository, UserManager<User> userManager)
+        {
+            _repository = repository;
+            _userManager = userManager;
+        }
+        
+        public async Task AddUser(RegistrationViewModel model)
+        {
+            User user = model.MapToUser();
+            var result = await _userManager.CreateAsync(user, model.Password);
+            if (result.Succeeded)
+            {
+                await _userManager.AddToRoleAsync(user, "user");
+            }
+        }
+
+        public async Task DeleteUser(int id)
+        {
+            User user = await _repository.GetFirstOrDefaultByIdAsync(id);
+            if (user is not null)
+            {
+                _repository.Remove(user);
+                await _repository.SaveAsync();
+            }
+        }
+    }
+}
