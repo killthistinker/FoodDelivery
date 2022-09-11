306eebb9387a4627f2071a43a07d9d25d27e3c53
killthistinker <killthistinker@gmail.com>
2022-09-12 03:24:37 +0600

    #4 Добавил модель для отображения корзины

---
+++ FoodDelivery/Models/ViewModels/BasketViewModels/BasketViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace FoodDelivery.Models.ViewModels.BasketViewModels
+{
+    public class BasketViewModel
+    {
+        public Dish Dish { get; set; }
+        public int DishCount { get; set; }
+    }
+}
