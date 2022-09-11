cab3891e6098824283eaa47e95e61495d5f93a98
killthistinker <killthistinker@gmail.com>
2022-09-12 03:11:34 +0600

    #3 Добавил модель заведения, настроил маппер для него

---
+++ FoodDelivery/Helpers/PointMapper.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using FoodDelivery.Models;
+using FoodDelivery.Models.ViewModels;
+using FoodDelivery.Models.ViewModels.PointViewModels;
+
+namespace FoodDelivery.Helpers
+{
+    public static class PointMapper
+    {
+        public static Point MapToPoint(this PointViewModel self)
+        {
+            Point point = new Point
+            {
+                Description = self.Description,
+                Image = self.ImagePath,
+                Name = self.Name,
+                Dishes = new List<Dish>(),
+                Basket = new Basket()
+            };
+            return point;
+        }
+    }
+}
---
+++ FoodDelivery/Models/Point.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace FoodDelivery.Models
+{
+    public class Point
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Image { get; set; }
+        public string Description { get; set; }
+        public int BasketId { get; set; }
+        public Basket Basket { get; set; }
+        public List<Dish> Dishes { get; set; }
+
+        public Point()
+        {
+            Dishes = new List<Dish>();
+        }
+    }
+}
