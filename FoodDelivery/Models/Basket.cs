eba0699a70d3fc0e26d362dccaa7f2ca00f25f1d
killthistinker <killthistinker@gmail.com>
2022-09-12 03:17:21 +0600

    #4 Создал модель корзины

---
+++ FoodDelivery/Models/Basket.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace FoodDelivery.Models
+{
+    public class Basket
+    {
+        public int Id { get; set; }
+        public decimal Sum { get; set; }
+        public List<Order> Orders { get; set; }
+
+        public Basket()
+        {
+            
+            Orders = new List<Order>();
+        }
+    }
+}
