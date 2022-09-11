5cda603614666d680254846af61d5d420334e409
killthistinker <killthistinker@gmail.com>
2022-09-12 03:15:40 +0600

    #3 Добавил маппер для блюда, добавил viewModel для создания блюда

---
+++ FoodDelivery/Helpers/DishMapper.cs
@@ -0,0 +1,20 @@
+﻿using FoodDelivery.Models;
+using FoodDelivery.Models.ViewModels;
+
+namespace FoodDelivery.Helpers
+{
+    public static class DishMapper
+    {
+        public static Dish MapToDish(this DishViewModel self)
+        {
+            Dish dish = new Dish
+            {
+                Description = self.Description,
+                Name = self.Name,
+                Price = self.Pirce,
+                PointId = self.PointId
+            };
+            return dish;
+        }
+    }
+}
---
+++ FoodDelivery/Models/ViewModels/DishViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace FoodDelivery.Models.ViewModels
+{
+    public class DishViewModel
+    {
+        [Display(Name = "Name")]
+        [Required(ErrorMessage = "Поле обязательно для заполнения")]
+        [Remote("CheckAccountName", "AccountValidation", ErrorMessage = "Логин занят")]
+        public string Name { get; set; }
+        [Display(Name = "Description")]
+        public string Description { get; set; }
+
+        [Display(Name = "Price")]
+        [Required(ErrorMessage = "Поле обязательно для заполнения")]
+        public decimal Pirce { get; set; }
+
+        public int PointId { get; set; }
+    }
+}
